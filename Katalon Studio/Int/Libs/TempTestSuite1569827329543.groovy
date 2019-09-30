1a6ac156c452f28b88492d78e7e07c8d2e41dacf
thirubeyond <thirubeyond@gmail.com>
2019-09-30 14:35:47 +0530

    Add Katalon Test

---
+++ Katalon Studio/Int/Libs/TempTestSuite1569827329543.groovy
@@ -0,0 +1,35 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/IntPlan')
+
+suiteProperties.put('name', 'IntPlan')
+
+suiteProperties.put('description', 'IntegrationPlan')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\mthir\\Katalon Studio\\Int\\Reports\\20190930_123844\\IntPlan\\20190930_123844\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/IntPlan', suiteProperties, new File("C:\\Users\\mthir\\Katalon Studio\\Int\\Reports\\20190930_123844\\IntPlan\\20190930_123844\\testCaseBinding"))
---
+++ Katalon Studio/Int/Test Suites/IntPlan.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>IntegrationPlan</description>
+   <name>IntPlan</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f90ccee8-1a74-47af-a0c9-1890b473e23a</testSuiteGuid>
+   <testCaseLink>
+      <guid>fa21b92c-d4cd-4fde-b0ce-8fd73b771fce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/G_OpenClose</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
